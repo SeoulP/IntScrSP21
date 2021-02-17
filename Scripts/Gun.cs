cba618b4665becf247af07bcca41c2b393768b8f
SeoulP <55219779+SeoulP@users.noreply.github.com>
2021-02-17 15:36:08 -0600

    Added Gun
    Added gun with functionality of Pickup, Use and Drop

---
+++ Scripts/Gun.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    // This function will be called from the playerController
+    public void Pickup(Transform hand)
+    {
+            // Set the parent of the "weapon" to the hand.
+            gameObject.transform.SetParent(hand);
+            // Sets the location of the gun to the hand's vector.
+            transform.localPosition = Vector3.zero;
+            // Will rotate the weapon.
+            transform.localRotation = Quaternion.identity;
+            // Kinematic turns the physics off.
+            GetComponent<Rigidbody>().isKinematic = true;
+            // Turns off the collider for the weapon.
+            GetComponent<Collider>().enabled = false;
+    }
+
+    public void Use()
+    {
+        Debug.Log("<color=red>Pow!</color>");
+    }
+
+    public void Drop()
+    {
+        Debug.Log("Dropping our item!");
+        this.gameObject.transform.SetParent(null);
+        transform.Translate(0,0,2);
+        GetComponent<Rigidbody>().isKinematic = false;
+        GetComponent<Rigidbody>().AddForce(transform.forward * 10, ForceMode.Impulse);
+        GetComponent<Collider>().enabled = true;
+    }
+
+}
---
+++ Scripts/PlayerController.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerController : MonoBehaviour
+{
+    // This is the players hand
+    [SerializeField]
+    Transform hand;
+
+    Gun heldItem;
+    public int oxygenSupply = 0;
+    public int coins = 0;
+    bool crouch = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        crouching();
+
+        if(Input.GetButtonDown("Fire1"))
+        {
+            if(heldItem != null)
+            {
+                heldItem.Use();
+            }
+            else
+            {
+                Debug.Log("We aren't holding anything!");
+            }
+        }
+
+        if(Input.GetKeyDown(KeyCode.Q))
+        {
+            if(heldItem != null)
+            {
+                heldItem.Drop();
+                heldItem = null;
+            }
+            else
+            {
+                Debug.Log("We aren't holding anything!");
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("I have hit " + other.gameObject.name + "!");
+
+        if (other.gameObject.CompareTag("Weapon"))
+        {
+            heldItem = other.GetComponent<Gun>();
+            heldItem.Pickup(hand);
+        }
+
+        if(other.gameObject.CompareTag("Pickup"))
+        {
+            Destroy(other.gameObject);
+            oxygenSupply++;
+            coins += 1;
+        }
+
+        if (other.gameObject.CompareTag("Floor")) {
+            other.gameObject.GetComponent<Renderer>().material.color = Random.ColorHSV();
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Floor"))
+        {
+            other.gameObject.GetComponent<Renderer>().material.color = Color.black;
+
+        }
+    }
+    void crouching()
+    {
+        if (Input.GetKeyDown(KeyCode.C) && crouch == false)
+        {
+            transform.localScale = new Vector3(1, .5f, 1);
+            crouch = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.C) && crouch == true)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+            crouch = false;
+        }
+    }
+}
